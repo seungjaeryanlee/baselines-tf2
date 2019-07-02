ac9aa76fc0b6ba42c6b3c651113f125ac144c069
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-07-02 13:13:18 +0900

    Copy baselines.acktr.utils.dense

--- baselines/gail/mlp_policy.py
+++ baselines/gail/mlp_policy.py
@@ -9,7 +9,27 @@ import gym
 import baselines.common.tf_util as U
 from baselines.common.mpi_running_mean_std import RunningMeanStd
 from baselines.common.distributions import make_pdtype
-from baselines.acktr.utils import dense
+# from baselines.acktr.utils import dense
+
+
+# TODO(seungjaeryanlee): This function is copied from OpenAI Baselines TF1 acktr
+def dense(x, size, name, weight_init=None, bias_init=0, weight_loss_dict=None, reuse=None):
+    with tf.variable_scope(name, reuse=reuse):
+        assert (len(tf.get_variable_scope().name.split('/')) == 2)
+
+        w = tf.get_variable("w", [x.get_shape()[1], size], initializer=weight_init)
+        b = tf.get_variable("b", [size], initializer=tf.constant_initializer(bias_init))
+        weight_decay_fc = 3e-4
+
+        if weight_loss_dict is not None:
+            weight_decay = tf.multiply(tf.nn.l2_loss(w), weight_decay_fc, name='weight_decay_loss')
+            if weight_loss_dict is not None:
+                weight_loss_dict[w] = weight_decay_fc
+                weight_loss_dict[b] = 0.0
+
+            tf.add_to_collection(tf.get_variable_scope().name.split('/')[0] + '_' + 'losses', weight_decay)
+
+        return tf.nn.bias_add(tf.matmul(x, w), b)
 
 
 class MlpPolicy(object):
